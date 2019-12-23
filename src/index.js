7cb4f01db816f758d669597679bdc598f3315c2a
VladYurash <vlad.yurash@gmail.com>
2019-12-23 23:08:55 +0300

    fuck this shit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import {createStore} from 'redux';
 
+const reducer = (state = 0, action) => {	
+
+	switch (action.type) {
+		case 'INC':
+			return state + 1;
+
+		case 'DEC':
+			return state - 1;	
+
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+
+let myState = null;
+
+store.subscribe(() => myState = store.getState());
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'DEC'});
+
+console.log(myState);
